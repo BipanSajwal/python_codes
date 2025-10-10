337dd12383c8ff1a16516bb6b12b475a9e0c867f
BipanSajwal <bipansajwal@gmail.com>
2025-10-11 00:59:12 +0100

    Initial Commit

---
+++ Bitwise_Operator.py
@@ -0,0 +1,11 @@
+#And& x&y , | Or x|y , XOR^ x^y
+# integers will be turned into binary numbers and perform the operations
+# all calculations works basis on truth table
+a = 99
+b=39
+print(bin(a))
+print(bin(b))
+print(bin(a&b),a&b)
+print(bin(a^b),a^b)
+print(bin(a|b),bin(a^b))
+
