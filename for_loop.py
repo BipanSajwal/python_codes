1560996cacf79ed33d94eced8a04f91ba79d7f8a
BipanSajwal <bipansajwal@gmail.com>
2025-10-19 00:53:22 +0100

    Initial Commit

---
+++ for_loop.py
@@ -0,0 +1,27 @@
+for n in range(1,101):
+   print(n,"I love you")
+
+for i in range (1,11):
+   print("10 *",i,"=",10*i)
+
+
+
+for j in range (100,0,-3):
+    print(j)
+
+
+for i in range (0,1000,1):
+    if i%2==0:
+        print(i,"even number")
+    else:
+        print(i,"odd number")
+
+
+a=("Bipan")
+for a in range(20):
+    print(a)
+
+
+for n in range(3):
+        b= input(f"Enter something for {n}: ")
+        print(b)
