2add88df13bcae66d5798a4be6c5b204d7192793
BipanSajwal <bipansajwal@gmail.com>
2025-10-21 02:09:42 +0100

    Initial Commit

--- global_variables.py
+++ global_variables.py
@@ -1,7 +1,7 @@
-x="hard"
+x="hard" #global variable
 def fun():
-    global x
-    x="easy"
-    print(f"python is {x}")
+    global x # it replaces loval variable value to global variable value
+    x="easy" #local variable
+    print(f"python is {x}") # this prints inside function
 fun()
-print(f"Python is {x}")
+print(f"Python is {x}")# this prints outside function after changed local to global variable
