157d4364b877e0e41b6de53907710e4e5351bf0b
BipanSajwal <bipansajwal@gmail.com>
2025-11-06 01:07:32 +0000

    Initial Commit

--- While_loop.py
+++ While_loop.py
@@ -3,18 +3,18 @@
 # condition
 # increment /decrement
 from comparision_operators import sajwal
-
-#a=15
-#while a>1:
- #   a-=1
-  #  print(a,"Hello")
-#print(a)
-
-#i = 0
-#while i < 11:
- #       print(i, "welcome to python series")
-  #      i += 1
-#print(i)
-
-
-
+#
+# a=15
+# while a>1:
+#     a-=1
+#     print(a,"Hello")
+# print(a)
+#
+# i = 0
+# while i < 11:
+#        print(i, "welcome to python series")
+#       i += 1
+# print(i)
+#
+#
+#
--- for_loop.py
+++ for_loop.py
@@ -1,49 +1,51 @@
 # for b in range(1,11):
-#     if b %2==0:
-#         print(b, end=" ")
+#      if b %2==0:
+#          print(b, end=" ")
 #
-# for n in range(1, 101):
-#             print(n, "I love you")
-#
-# for i in range(1, 11):
-#             print("10 *", i, "=", 10 * i)
-#
-# for j in range(100, 0, -3):
-#             print(j)
-#
-# for i in range(0, 1000, ):
-#             if i % 2 == 0:
-#                 print(i, "even number")
-#             else:
-#                 print(i, "odd number")
-# a = "Bipan"
-# for a in range(20):
-#             print(a)
-# for n in range(1):
-#             b = input(f"Enter something for {n}: ")  # uses an f-string to show the current number in the code
-#             print(b)
-#
-# for b in range(100):
-#             print(b, "Bipan")
-#
-# for he in range(1, 6):
-#             print(he, he ** 2)
+# for n in range(1,101):
+#     print(n,"I Love You ")
 #
 #
-# total = 0
-# for i in range(1, 11):
-#     total += i
-# print("Sum of first 10 numbers is ", total,end=" ")
+#
+#  for i in range(1, 11):
+#              print("10 *", i, "=", 10 * i)
+#
+#  for j in range(100, 0, -3):
+#              print(j)
+#
+#  for i in range(0, 1000, ):
+#              if i % 2 == 0:
+#                  print(i, "even number")
+#              else:
+#                  print(i, "odd number")
+#  a = "Bipan"
+#  for a in range(20):
+#              print(a)
+#  for n in range(1):
+#              b = input(f"Enter something for {n}: ")  # uses an f-string to show the current number in the code
+#              print(b)
+#
+#  for b in range(100):
+#              print(b, "Bipan")
+#
+#  for he in range(1, 6):
+#              print(he, he ** 2)
 #
 #
-# hey=0
-# for i in range (1,5):
-#     hey=i**5
-#     print(hey)
+#  total = 0
+#  for i in range(1, 11):
+#      total += i
+#  print("Sum of first 10 numbers is ", total,end=" ")
 #
 #
-
-
-a=["Bipan","Sajwal","Python","Coding"]
-for i in (a):
-    print(i)
+#  hey=0
+#  for i in range (1,5):
+#      hey=i**5
+#      print(hey)
+#
+#
+#
+# a=["Bipan","Sajwal","Python","Coding"]
+# for i in (a):
+#     print(i)
+#
