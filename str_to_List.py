c74b66f7c89b292ca93107209c2dd858a1922808
BipanSajwal <bipansajwal@gmail.com>
2025-11-06 01:33:40 +0000

    Initial Commit

---
+++ str_to_List.py
@@ -0,0 +1,13 @@
+# from os.path import split
+#
+# a=[]
+# for i in range (1,4):
+#     p= (input("Enter the Colour:"))
+#     a.append(p)
+# print(a)
+
+
+
+a=(input("Enter The Sentence:"))
+c=a.split()
+print(c)
